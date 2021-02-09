27d176a6ca63a1699c809c071b91b89df7990bce
Ermesson Lima <ermessonlimadossantos@hotmail.com>
2021-02-09 17:03:02 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,6 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: 'https://api.github.com',
-    client_id: "2708927d775286a82e36",
-    client_secret: "b06f811efa13106510931ae69d20df6adf267fba"
 });
 
 export default api;
